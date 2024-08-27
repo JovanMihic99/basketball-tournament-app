0ccd7e686457721bd3e419a54aae06c3acdafc10
JovanMihic99 <jovanmihic99@gmail.com>
2024-08-27 22:05:02 +0200

    feat: add initializeStandings() function and add printGroups() function for better readability

--- src/groups.js
+++ src/groups.js
@@ -5,10 +5,48 @@ let groups = {};
 
 exports.simulateGroupPhase = async () => {
   groups = await util.readJSONFile("./groups.json");
+
   console.log("GRUPNA FAZA:");
-  for (const key in groups) {
-    console.log(groups[key]);
-  }
+  intializeStandings(groups);
+  printGroups(groups);
 };
 
-function simulateMatch(team1, team2) {}
+function simulateMatch(team1, team2) {
+  const rankDifferenece = team1.rank - tean2.rank;
+}
+
+function intializeStandings(groups) {
+  Object.keys(groups).forEach((group) => {
+    groups[group] = groups[group].map((team) => ({
+      ...team,
+      wins: 0,
+      losses: 0,
+      pointsScored: 0,
+      pointsConceded: 0,
+      totalPoints: 0,
+    }));
+  });
+  return groups;
+}
+
+function printGroups(groups) {
+  console.log("Grupe:");
+
+  Object.keys(groups).forEach((group) => {
+    console.log(`Grupa ${group}:`);
+
+    groups[group].forEach((team) => {
+      console.log(
+        `  ${team.Team} (ISO Code: ${team.ISOCode}, FIBA Ranking: ${
+          team.FIBARanking
+        }, Wins: ${team.wins || 0}, Losses: ${
+          team.losses || 0
+        }, Points Scored: ${team.pointsScored || 0}, Points Conceded: ${
+          team.pointsConceded || 0
+        }, Total Points: ${team.totalPoints || 0})`
+      );
+    });
+
+    console.log("");
+  });
+}
