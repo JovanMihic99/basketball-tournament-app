d6be3919856fc8fb05c066cc2ade6ffd8e245f55
JovanMihic99 <jovanmihic99@gmail.com>
2024-08-27 23:31:16 +0200

    feat: add simulateAllGroupMatches(groups) function
    Simulates group phase using a round robin algorithm for each team, in it's respective group, for updating standings (wins and losses)

--- src/groups.js
+++ src/groups.js
@@ -10,10 +10,58 @@ exports.simulateGroupPhase = async () => {
   groups = intializeStandings(groups); // add standings fields to the every team in groups
 
   printGroups(groups);
+  SimulateAllGroupMatches(groups);
+  printGroups(groups);
 };
 
-function simulateMatch(team1, team2) {
-  const rankDifferenece = team1.rank - team2.rank;
+function SimulateAllGroupMatches(groups) {
+  Object.keys(groups).forEach((group) => {
+    const teams = groups[group];
+
+    for (let i = 0; i < teams.length; i++) {
+      for (let j = i + 1; j < teams.length; j++) {
+        const team1 = teams[i];
+        const team2 = teams[j];
+
+        const result = simulateMatch(team1, team2);
+        console.log(
+          `group ${group}, i, j: (${i}, ${j})), ${team1.ISOCode} Vs ${team2.ISOCode} | Winner: ${result.winner.ISOCode}`
+        );
+        // update winner
+        updateTeamStandings(group, result.winner.ISOCode, {
+          wins: result.winner.wins + 1,
+        });
+        // update loser
+        updateTeamStandings(group, result.loser.ISOCode, {
+          losses: result.loser.losses + 1,
+        });
+      }
+    }
+  });
+
+  function simulateMatch(team1, team2) {
+    winner = team1;
+    loser = team2;
+    if (Math.random() > 0.5) {
+      winner = team2;
+      loser = team1;
+    }
+    return { winner, loser };
+  }
+  function updateTeamStandings(group, isoCode, newValue) {
+    const groupArray = groups[group];
+    if (!groupArray) {
+      console.error("No group found!");
+    }
+
+    const team = groupArray.find((team) => team.ISOCode === isoCode);
+
+    if (team) {
+      Object.assign(team, newValue);
+    } else {
+      console.error("No team found!");
+    }
+  }
 }
 
 function intializeStandings(groups) {
@@ -22,8 +70,6 @@ function intializeStandings(groups) {
       ...team,
       wins: 0,
       losses: 0,
-      pointsScored: 0,
-      pointsConceded: 0,
       totalPoints: 0,
     }));
   });
@@ -39,7 +85,7 @@ function printGroups(groups) {
 
     groups[group].forEach((team) => {
       console.log(
-        `  ${team.Team} (ISO Code: ${team.ISOCode}, FIBA Ranking: ${team.FIBARanking}, Wins: ${team.wins}, Losses: ${team.losses}, Points Scored: ${team.pointsScored}, Points Conceded: ${team.pointsConceded}, Total Points: ${team.totalPoints})`
+        `  ${team.Team} (ISO Code: ${team.ISOCode}, FIBA Ranking: ${team.FIBARanking}, Wins: ${team.wins}, Losses: ${team.losses}, Total Points: ${team.totalPoints})`
       );
     });
 
