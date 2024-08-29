08f58140a6cffc484b18e85ee39fc724aaec4cab
JovanMihic99 <jovanmihic99@gmail.com>
2024-08-29 20:50:50 +0200

    feat: add simple drawQuarterFinalists()
    only does simple random draw, need to add functionality to check if teams have aleready faced eachother

--- src/controllers/quarterFinalController.js
+++ src/controllers/quarterFinalController.js
@@ -7,18 +7,20 @@ let pots = {
 exports.simulateQuarterFinals = (groups) => {
   drawTeams(groups);
   printDrawResults();
+  drawQuarterFinalists();
   // console.log(groups);
 };
 
 function drawTeams(groups) {
-  let quarerFinalists = [];
+  let teamsArray = [];
+  let quarterFinalists = [];
   for (const group in groups) {
     const el = groups[group];
 
     el.forEach((team) => {
       if (team.rank !== null && team.rank <= 8) {
         // put quarter finalists into an array
-        quarerFinalists.push({
+        teamsArray.push({
           ISOCode: team.ISOCode,
           name: team.Team,
           rank: team.rank,
@@ -26,16 +28,32 @@ function drawTeams(groups) {
       }
     });
   }
-  quarerFinalists = quarerFinalists.sort((a, b) => a.rank - b.rank);
+  teamsArray = teamsArray.sort((a, b) => a.rank - b.rank);
 
-  pots.D = [quarerFinalists[0], quarerFinalists[1]];
-  pots.E = [quarerFinalists[2], quarerFinalists[3]];
-  pots.F = [quarerFinalists[4], quarerFinalists[5]];
-  pots.G = [quarerFinalists[6], quarerFinalists[7]];
+  pots.D = [teamsArray[0], teamsArray[1]];
+  pots.E = [teamsArray[2], teamsArray[3]];
+  pots.F = [teamsArray[4], teamsArray[5]];
+  pots.G = [teamsArray[6], teamsArray[7]];
+}
+
+function drawQuarterFinalists() {
+  // ...to be finished
+  let quarterFinals = [];
+
+  console.log(Math.round(Math.random()));
+  quarterFinals.push({
+    team1: pots.D[Math.round(Math.random())],
+    team2: pots.G[Math.round(Math.random())],
+  });
+  quarterFinals.push({
+    team1: pots.E[Math.round(Math.random())],
+    team2: pots.F[Math.round(Math.random())],
+  });
+  console.log(quarterFinals);
 }
 
 function printDrawResults() {
-  console.log("Šeširi:");
+  console.log("\nŠeširi:");
   console.log(`  Šešir D\n    ${pots.D[0].name}\n    ${pots.D[1].name}`);
   console.log(`  Šešir E\n    ${pots.E[0].name}\n    ${pots.E[1].name}`);
   console.log(`  Šešir F\n    ${pots.F[0].name}\n    ${pots.F[1].name}`);
