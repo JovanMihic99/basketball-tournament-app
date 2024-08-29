d81078b5786bafde76e35bb7b13f662ee5c878f1
JovanMihic99 <jovanmihic99@gmail.com>
2024-08-29 20:30:07 +0200

    feat: add printing of drawn teams functionality

--- src/controllers/quarterFinalController.js
+++ src/controllers/quarterFinalController.js
@@ -6,6 +6,7 @@ let pots = {
 };
 exports.simulateQuarterFinals = (groups) => {
   drawTeams(groups);
+  printDrawResults();
   // console.log(groups);
 };
 
@@ -19,6 +20,7 @@ function drawTeams(groups) {
         // put quarter finalists into an array
         quarerFinalists.push({
           ISOCode: team.ISOCode,
+          name: team.Team,
           rank: team.rank,
         });
       }
@@ -30,6 +32,12 @@ function drawTeams(groups) {
   pots.E = [quarerFinalists[2], quarerFinalists[3]];
   pots.F = [quarerFinalists[4], quarerFinalists[5]];
   pots.G = [quarerFinalists[6], quarerFinalists[7]];
+}
 
-  console.log(pots);
+function printDrawResults() {
+  console.log("Šeširi:");
+  console.log(`  Šešir D\n    ${pots.D[0].name}\n    ${pots.D[1].name}`);
+  console.log(`  Šešir E\n    ${pots.E[0].name}\n    ${pots.E[1].name}`);
+  console.log(`  Šešir F\n    ${pots.F[0].name}\n    ${pots.F[1].name}`);
+  console.log(`  Šešir G\n    ${pots.G[0].name}\n    ${pots.G[1].name}`);
 }
